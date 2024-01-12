c969eb012fe0b7885ad9d4da62432584b415d17b
Akhmad Salafudin <axmad386@gmail.com>
2024-01-12 09:39:04 +0700

    test(url): defaultPhoneNumber diganti menjadi defaultPhoneNumberId

--- test/url.test.ts
+++ test/url.test.ts
@@ -4,14 +4,16 @@ describe("test wa url generator",()=>{
   it("can generate SEND_MESSAGE url",()=>{
     const wa = new Wa({
       token: "token",
-      defaultPhoneNumberId: "phone-number-id"
+      defaultPhoneNumberId: "phone-number-id",
+      accountId: "account-id"
     })
     expect(wa.url.SEND_MESSAGE).toBe("https://graph.facebook.com/v18.0/phone-number-id/messages")
   })
   it("can change phone number", ()=>{
     const wa = new Wa({
       token: "token",
-      defaultPhoneNumberId: "phone-number-id"
+      defaultPhoneNumberId: "phone-number-id",
+      accountId: "account-id"
     })
     wa.url.setPhoneNumberId("xxx")
     expect(wa.url.getPhoneNumberId()).toBe("xxx")
